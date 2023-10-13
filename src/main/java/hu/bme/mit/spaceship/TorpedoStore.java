d85186d3da7721ad5d905e208a292ba9cda69395
Nagy Benedek <48951990+NBiTech@users.noreply.github.com>
2023-10-13 13:48:25 +0200

    Revert "Fix_lab3"

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -8,10 +8,9 @@ import java.util.Random;
 * (Deliberately contains bugs.)
 */
 public class TorpedoStore {
-  
+
   // rate of failing to fire torpedos [0.0, 1.0]
   private double FAILURE_RATE = 0.0; //NOSONAR
-  private Random generator = new Random();
 
   private int torpedoCount = 0;
 
@@ -31,17 +30,18 @@ public class TorpedoStore {
 
   public boolean fire(int numberOfTorpedos){
     if(numberOfTorpedos < 1 || numberOfTorpedos > this.torpedoCount){
-      throw new IllegalArgumentException("numberOfTorpedos");
+      new IllegalArgumentException("numberOfTorpedos");
     }
 
     boolean success = false;
 
     // simulate random overheating of the launcher bay which prevents firing
+    Random generator = new Random();
     double r = generator.nextDouble();
 
     if (r >= FAILURE_RATE) {
       // successful firing
-      this.torpedoCount -= numberOfTorpedos;
+      this.torpedoCount =- numberOfTorpedos;
       success = true;
     } else {
       // simulated failure
@@ -58,5 +58,4 @@ public class TorpedoStore {
   public int getTorpedoCount() {
     return this.torpedoCount;
   }
-  //Here volt hiba
 }
